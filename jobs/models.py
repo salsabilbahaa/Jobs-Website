13d6b04de812e7fc34a37c1e1df307d995555c8d
salsabil <salsabilbahaa2005@gmail.com>
2025-05-17 02:09:08 +0300

    model, view, url job app

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    STATUS_CHOICES = [
+        ('open', 'Open'),
+        ('close', 'Closed'),
+    ]
+    title = models.CharField(max_length=100)
+    salary = models.IntegerField(max_digits=10)
+    company = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.title
+
+class Application(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    date_applied = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.job.title} - {self.date_applied}"
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Job, Application
 
-# Create your views here.
+def search_jobs(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/search_jobs.html', {'jobs': jobs})
+
+def apply_to_job(request, job_id):
+    job = Job.objects.get(pk=job_id)
+    Application.objects.create(job=job)
+    return redirect('my_applications')
+
+def my_applications(request):
+    applications = Application.objects.all()
+    return render(request, 'jobs/my_applications.html', {'applications': applications})
